96728f2854727f92408d4b656c8497599043d69d
tisonpatrik <patriktison@gmail.com>
2025-12-02 00:33:16 +0100

    day01

---
+++ src/Day01.kt
@@ -0,0 +1,29 @@
+data class Instructions(val direction: Char, val distance: Int)
+
+fun main() {
+    val listOfInstructions: List<Instructions> = readInput("Day01")
+        .filter { it.isNotBlank() }
+        .map { radek ->
+            val direction = radek[0]
+            val distance = radek.drop(1).trim().toInt()
+            Instructions(direction, distance)
+        }
+
+    val dialSize = 100
+    val targetNumber = 0
+
+    var currentPosition = 50
+    var targetHitCount = 0
+
+    for (inst in listOfInstructions) {
+        val step = if (inst.direction == 'R') 1 else -1
+        repeat(inst.distance) {
+            currentPosition = (currentPosition + step).mod(dialSize)
+            if (currentPosition == targetNumber) {
+                targetHitCount++
+
+            }
+        }
+    }
+    println(targetHitCount)
+}
